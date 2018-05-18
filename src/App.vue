fceacadeec764bad1927604135f76e2be6e717ff
zysallen <243816351@qq.com>
2018-05-18 18:59:42 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -113,7 +113,7 @@ const nebPay = new NebPay();
 const Account = nebulas.Account;
 const neb = new nebulas.Neb();
 const from = Account.NewAccount().getAddressString();
-const to = "n1qQfgZG5aGUM5WYne4dwfcFqBC2hV4hFLz";
+const to = "n1y3oFjdEa9XBWcsuZryo7B6gDkGRjzz83V";
 neb.setRequest(new nebulas.HttpRequest("https://testnet.nebulas.io"));
 
 neb.api.getNebState().then(res => {
